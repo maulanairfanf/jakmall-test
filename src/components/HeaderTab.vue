64058526f3fceccbf1188b718cbac2dab1aa6d58
maulanairfanf <maulanairfanf@gmail.com>
2024-02-27 23:55:39 +0700

    add navigation tab and header tab

---
+++ src/components/HeaderTab.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+	title: {
+		type: String,
+		required: true,
+		default: '',
+	},
+})
+</script>
+<template>
+	<h1 class="text_primary">
+		{{ title }}
+		<div class="underline"></div>
+	</h1>
+</template>
---
+++ src/components/NavigationTab.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ChevronRightIcon } from '@heroicons/vue/24/solid'
+
+defineProps({
+	step: {
+		default: 1,
+		type: Number,
+		required: true,
+	},
+})
+</script>
+<template>
+	<div class="flex flex_justify_center">
+		<div class="header_navigation flex flex_justify_center flex_items_center">
+			<div class="flex flex_items_center margin_right_10">
+				<div
+					class="circle_navigation margin_right_10"
+					:class="step >= 1 && 'circle_navigation_active'"
+				>
+					<span>1</span>
+				</div>
+				<span class="text_primary margin_right_10">Delivery</span>
+				<ChevronRightIcon class="text_primary icon" />
+			</div>
+			<div class="flex flex_items_center margin_right_10">
+				<div
+					class="circle_navigation margin_right_10"
+					:class="step >= 2 && 'circle_navigation_active'"
+				>
+					<span>2</span>
+				</div>
+				<span class="text_primary margin_right_10">Delivery</span>
+				<ChevronRightIcon class="text_primary icon" />
+			</div>
+			<div class="flex flex_items_center margin_right_10">
+				<div
+					class="circle_navigation margin_right_10"
+					:class="step >= 3 && 'circle_navigation_active'"
+				>
+					<span>3</span>
+				</div>
+				<span class="text_primary margin_right_10">Delivery</span>
+			</div>
+		</div>
+	</div>
+</template>
